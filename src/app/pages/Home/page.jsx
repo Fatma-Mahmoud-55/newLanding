def3c20f33db115f971a66ac34f512af4061ea52
Fatma-Mahmoud-55 <113183957+Fatma-Mahmoud-55@users.noreply.github.com>
2025-08-01 13:44:14 +0300

    section six fix

--- src/app/pages/Home/page.jsx
+++ src/app/pages/Home/page.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Hero from '../../../compnents/LandingSections/Hero'; // Fixed "compnents" typo
-import SectionOne from '../../../compnents/LandingSections/SectionOne'; // Fixed "compnents" typo
 import SectionSix from '../../../compnents/LandingSections/SectionSix'; // Fixed "compnents" typo
 import SectionSixV2 from '../../../compnents/LandingSections/SectionSixV2'; // Fixed "compnents" typo
 import SectionsContainerOne from '../../../compnents/LandingSections/SectionsContainerOne'; // Fixed "compnents" typo
@@ -12,7 +11,7 @@ export default function Page() {  // Changed to default export and PascalCase
        <Hero />
        <SectionsContainerOne />
        <SectionSix />
-       <SectionSixV2 />
+       {/* <SectionSixV2 /> */}
       </div>
     </>
   );
